93a8805401bd7fdba2c7047866b0a00903d5aa4d
satish1110 <rsatish57@gmail.com>
2021-10-03 00:42:29 +0530

    3rd flask

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template , request  
+from flasgger import Swagger 
+
 
 #WSGI 
 app = Flask(__name__)
+Swagger(app)
 
 # mention request method, default GET 
 @app.route('/', methods = ['GET'])  
@@ -9,17 +12,35 @@ app = Flask(__name__)
 # asynchronously 
 
 def welcome():
-    return render_template("index.html")
+    return "Learning swagger"
 
 @app.route('/name/<your_name>')
 def names(your_name):
     return f"Welcome {your_name}"
 
-@app.route('/check_request')
+@app.route('/check_request', methods = ['POST', 'GET'])
 def get_req_parameters():
-    name = request.args.get("name")
-    roll = request.args.get("roll")
-    return f"{name} {roll} studies in praxis", 206 
+
+    """Practicing swagger
+    ---
+    parameters:
+        - name: Student_name
+          in: query
+          type: string
+          required: true
+        - name: roll_no
+          in: query
+          type: number
+          required: true
+    responses:
+        200:
+            description: result is 
+     """
+    name = request.args.get("Student_name")
+    roll = request.args.get("roll_no")
+
+    # MESSAGE AND RESPONSE 
+    return f"{name} {roll} studies in praxis", 200
 
 if __name__ == "__main__":
     app.run(debug=True, port = "8091")
